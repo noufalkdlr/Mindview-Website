ba2e172294dc5b4c277ca3b50a82cb4c7ee8cb4b
Noufal <noufalkakdlr@gmail.com>
2025-01-08 15:58:30 +0530

    about

--- mindview/main/views.py
+++ mindview/main/views.py
@@ -6,3 +6,6 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'main/html/index.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'main/html/about.html'
